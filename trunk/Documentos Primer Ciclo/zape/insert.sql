94b31e56eb5659c67e5b50985105ade114ae01f9
davidg20@gmail.com <davidg20@gmail.com@4086ce8d-edd2-8dd7-b54f-3daf8c2219de>
2013-05-28 01:52:59 +0000

    los scripts finales

---
+++ trunk/Documentos Primer Ciclo/zape/insert.sql
@@ -0,0 +1,158 @@
+﻿INSERT INTO documentos(nombre,archivo) VALUES ('00_Documento', '00_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('01_Documento', '01_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('02_Documento', '02_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('03_Documento', '03_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('04_Documento', '04_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('05_Documento', '05_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('06_Documento', '06_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('07_Documento', '07_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('08_Documento', '08_Archivo');
+INSERT INTO documentos(nombre,archivo) VALUES ('09_Documento', '09_Archivo');
+---------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES(1059784989,'C.C.','DAVID','ARROYAVE','CALLE 65 # 30-17','8875841','davidg20@gmail.com','Admin','arrendador','2008/12/21');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES(1105455450,'C.C.','ESTEFANIA','MACIAS','CARREARA 25 #56-119','8858217','estefaniamacias@gmail.com','mundial2011','arrendatario','2009/10/11');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES(1000000002,'C.C.','Juan Camilo','Varela López','02_Dir','8851217','juan55860@gmail.com','02_con','arrendador','2010/12/12');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES(1000000003,'C.C.','Carla','Zuluaga','03_Dir','8851122','karlazuluaga24@gmail.com','03_con','arrendatario','2010/02/21');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES (1000000004,'C.C.','Sandro','Gonzalez','04_Dir','8872121','sandrouniversidad2011@gmail.com','04_con','arrendador','2009/11/21');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES (1000000005,'C.C.','David Leonardo','Agudelo Villa','05_Dir','8851234','daviko.50@gmail.com','05_con','arrendatario','2011/01/19');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES (1000000006,'C.C.','martha liliana','gomez mejia','06_Dir','8851432','mgliliana1028@gmail.com','06_con','arrendador','2011/01/30');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES (1000000007,'C.C.','Andres Humberto','Hernandez Gonzalez','07_Dir','8872212','andreshumberto619@gmail.com','07_con','arrendatario','2012/03/18');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES (1000000008,'C.C.','Juan','Rendon','08_Dir','8557212','jcrendon@gmail.com','08_con','arrendador','2012/04/18');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro)
+VALUES (1000000009,'C.C.','alejandra','ospina aguirre','09_Dir','8879213','aleopig@gmail.com','09_con','arrendatario','2012/04/22');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000010,'C.C.','10_Nom','10_Ape','10_Dir','10_Tel','10@00.00','10_con','arrendador','2012/05/12');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000011,'C.C.','11_Nom','11_Ape','11_Dir','11_Tel','11@00.00','11_con','arrendatario','2012/09/12');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000012,'C.C.','12_Nom','12_Ape','12_Dir','12_Tel','12@00.00','12_con','arrendador','2012/09/29');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000013,'C.C.','13_Nom','13_Ape','13_Dir','13_Tel','13@00.00','13_con','arrendatario','2012/10/09');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000014,'C.C.','14_Nom','14_Ape','14_Dir','14_Tel','14@00.00','14_con','arrendador','2012/10/19');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000015,'C.C.','15_Nom','15_Ape','15_Dir','15_Tel','15@00.00','15_con','arrendatario','2012/10/29');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000016,'C.C.','16_Nom','16_Ape','16_Dir','16_Tel','16@00.00','16_con','arrendador','2012/10/30');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000017,'C.C.','17_Nom','17_Ape','17_Dir','17_Tel','17@00.00','17_con','arrendatario','2012/11/10');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000018,'C.C.','18_Nom','18_Ape','18_Dir','18_Tel','18@00.00','18_con','arrendador','2012/11/11');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000019,'C.C.','19_Nom','19_Ape','19_Dir','19_Tel','19@00.00','19_con','arrendatario','2012/11/21');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000020,'C.C.','20_Nom','20_Ape','20_Dir','20_Tel','20@00.00','20_con','administrador','2012/11/30');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000021,'C.C.','21_Nom','21_Ape','21_Dir','21_Tel','21@00.00','21_con','administrador','2012/12/01');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000022,'C.C.','22_Nom','22_Ape','22_Dir','22_Tel','22@00.00','22_con','administrador','2012/12/11');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000023,'C.C.','23_Nom','23_Ape','23_Dir','23_Tel','23@00.00','23_con','administrador','2012/12/16');
+INSERT INTO personas(identificacion,tipo_identificacion,nombre,apellido,direccion,telefono,correo,contrasenna,rol,fecha_registro) VALUES (1000000024,'C.C.','24_Nom','24_Ape','24_Dir','24_Tel','24@00.00','24_con','administrador','2012/12/21');
+---------------------------------------------------------------------------------------------------------------------------------------------------
+--Comunas:
+INSERT INTO barrios(nombre) VALUES ('Chipre');
+INSERT INTO barrios(nombre) VALUES ('Lucitania');
+INSERT INTO barrios(nombre) VALUES ('Enea');
+INSERT INTO barrios(nombre) VALUES ('San Jorge');
+INSERT INTO barrios(nombre) VALUES ('La Argentina');
+INSERT INTO barrios(nombre) VALUES ('Laureles');
+INSERT INTO barrios(nombre) VALUES ('La Carola');
+INSERT INTO barrios(nombre) VALUES ('Fatima');
+INSERT INTO barrios(nombre) VALUES ('Colinas');
+INSERT INTO barrios(nombre) VALUES ('Kenedy');
+INSERT INTO barrios(nombre) VALUES ('Villa pilar');
+INSERT INTO barrios(nombre) VALUES ('El bosque');
+INSERT INTO barrios(nombre) VALUES ('Aranjuez');
+INSERT INTO barrios(nombre) VALUES ('La Fuente');
+INSERT INTO barrios(nombre) VALUES ('Villa Carmenza');
+INSERT INTO barrios(nombre) VALUES ('Palermo');
+INSERT INTO barrios(nombre) VALUES ('Milan');
+INSERT INTO barrios(nombre) VALUES ('Arboleda');
+INSERT INTO barrios(nombre) VALUES ('Centro');
+INSERT INTO barrios(nombre) VALUES ('Campoamor');
+INSERT INTO barrios(nombre) VALUES ('Fundadores');
+INSERT INTO barrios(nombre) VALUES ('Hoyo frío');
+INSERT INTO barrios(nombre) VALUES ('Las americas');
+INSERT INTO barrios(nombre) VALUES ('Los agustinos');
+INSERT INTO barrios(nombre) VALUES ('La palma');
+--Corregimientos:
+INSERT INTO barrios(nombre) VALUES ('Belmira');
+INSERT INTO barrios(nombre) VALUES ('Buenavista');
+INSERT INTO barrios(nombre) VALUES ('Chupaderos');
+INSERT INTO barrios(nombre) VALUES ('El desquite');
+INSERT INTO barrios(nombre) VALUES ('El paraiso');
+INSERT INTO barrios(nombre) VALUES ('La esperanza');
+INSERT INTO barrios(nombre) VALUES ('La favorita');
+INSERT INTO barrios(nombre) VALUES ('Altamar');
+---------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO residenciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_pisos,parqueadero,cod_barrio) 
+VALUES(1800000,'CARRERA 3 #11-13',4,1105455450,2,800,'MUY BONITO','LIBRE','2012/01/21',TRUE,'Casa',3,8,2,TRUE,'bar111');
+INSERT INTO residenciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_pisos,parqueadero,cod_barrio) 
+VALUES(900000,'CARRERA 5 CALLE 10 Edificio Pinares #404',4,1105455450,5,200,'MUY BONITO','ARRENDADO','2012/02/11',TRUE,'Apartamento',2,3,1,TRUE,'bar111');
+INSERT INTO residenciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_pisos,parqueadero,cod_barrio) 
+VALUES(500000,'CARRERA 5 CALLE 11 #103',3,1105455450,5,50,'MUY BONITO','LIBRE','2012/03/09',TRUE,'Apartaestudio',1,1,1,FALSE,'bar111');
+
+INSERT INTO comerciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_pisos,cod_barrio) 
+VALUES(300000,'CALLE 65 #30-18',2,1000000002,2,20,'vende de todo','libre','2012/03/19',TRUE,'Local',1,1,'bar101');
+INSERT INTO comerciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_pisos,cod_barrio) 
+VALUES(450000,'CALLE 32 # 11-11',3,1000000004,3,80,'Que elegancia','Arrendado','2012/04/19',TRUE,'Oficina',1,1,'bar102');
+INSERT INTO comerciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_pisos,cod_barrio) 
+VALUES(800000,'Calle 19 #23-12',3,1000000006,1,120,'muy amplio','libre','2012/04/29',TRUE,'Consultorio',2,2,'bar103');
+INSERT INTO comerciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_pisos,cod_barrio) 
+VALUES(200000,'Carrera 25 #54-11',2,1000000004,4,500,'le cabe de todo','libre','2012/04/30',TRUE,'Bodega',1,1,'bar104');
+INSERT INTO comerciales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_pisos,cod_barrio) 
+VALUES(1500000,'CARRERA 22 # 25-11',4,1000000008,1,800,'Parquea lo que sea','Arrendado','2012/06/10',TRUE,'Parqueadero',2,10,'bar105');
+
+INSERT INTO rurales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_piscinas) 
+VALUES(120000,'kilometro 40',1,1000000002,5,2000,'tranquila','Arrendado','2012/06/22',TRUE,'finca',2,5,0);
+INSERT INTO rurales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_piscinas)
+VALUES(3000000,'La pintada',6,1000000010,2,300,'Rebonita','libre','2012/07/22',TRUE,'Casa campestre',3,6,2);
+INSERT INTO rurales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_piscinas)
+VALUES(250000,'3 letras',1,1000000012,4,150,'mucho frio','libre','2012/07/27',TRUE,'Cabana',1,1,0);
+INSERT INTO rurales(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo,num_bannos,num_habitaciones,num_piscinas)
+VALUES(2500000,'Via Neira',4,1000000014,4,350,'ReRebonita','Arrendado','2012/10/17',TRUE,'Casa campestre',3,8,1);
+
+INSERT INTO lotes(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo) VALUES(250000,'Via bar c',2,1000000018,3,200,'Pa lo que quiera','Arrendado','2012/10/21',TRUE,'Construccion');
+INSERT INTO lotes(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo) VALUES(400000,'Bar c pa arriba',2,1000000018,2,3000,'pega lo que le siembre','Libre','2012/11/01',TRUE,'Cultivo');
+INSERT INTO lotes(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,tipo) VALUES(1000000,'Villamaria para arriba',2,1000000016,1,5000,'Buenos pastos','Libre','2012/11/21',TRUE,'Ganaderia');
+
+INSERT INTO edificios(precio,direccion,estrato,cod_propietario,prioridad,area,descripcion,estado,fecha_registro,validacion,num_pisos,num_parqueaderos)
+VALUES(20000000,'CARRERA 24 #70-11',4,1000000016,8,1200,'muy alto','Arrendado','2013/01/19',TRUE,50,100);
+---------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('Juan Camilo','Varela López',8851217,'juan55860@gmail.com','inm101');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('Carla','Zuluaga',8851122,'karlazuluaga24@gmail.com','inm102');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('David','Arroyave',8875841,'davidg20@gmail.com','inm103');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('Sandro','Gonzalez',8872121,'sandrouniversidad2011@gmail.com','inm104');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('David Leonardo','Agudelo Villa',8851234,'daviko.50@gmail.com','inm105');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('martha liliana','gomez mejia',8851432,'mgliliana1028@gmail.com','inm106');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('Andres Humberto','Hernandez Gonzalez',8872212,'andreshumberto619@gmail.com','inm107');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble) 
+VALUES ('Alejandra','Ospina Aguirre',8879213,'aleopig@gmail.com','inm108');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('','09_Ape',09,'09@00.00','inm109');
+INSERT INTO solicitudes(nombre,apellido,telefono,correo,cod_inmueble)
+VALUES ('Alejandro','Arroyave Franco',09,'alejandrof@gmail.com','inm110');
+
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1059784989,1105455450,'00_Des','2010/01/01','inm101',00,00,6);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000002,1000000003,'01_Des','2012/01/01','inm101',01,01,8);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000004,1000000005,'02_Des','2012/02/01','inm102',02,02,4);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000006,1000000007,'03_Des','2012/03/01','inm103',03,03,03);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000008,1000000009,'04_Des','2012/04/01','inm104',04,04,04);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000010,1000000011,'05_Des','2012/05/01','inm105',05,05,05);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000012,1000000013,'06_Des','2012/06/01','inm106',06,06,06);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000014,1000000015,'07_Des','2012/07/01','inm107',07,07,07);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000016,1000000017,'08_Des','2012/08/01','inm108',08,08,08);
+INSERT INTO contratos(cod_arrendador,cod_arrendatario,descripcion,fecha,cod_inmueble,valor,comision,duracion) VALUES (1000000018,1000000019,'09_Des','2012/09/01','inm109',09,09,09);
+
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('01_Arc','inm101');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('02_Arc','inm102');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('03_Arc','inm103');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('04_Arc','inm104');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('05_Arc','inm105');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('06_Arc','inm106');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('07_Arc','inm107');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('08_Arc','inm108');
+INSERT INTO fotos(archivo,cod_inmueble) VALUES('09_Arc','inm109');
